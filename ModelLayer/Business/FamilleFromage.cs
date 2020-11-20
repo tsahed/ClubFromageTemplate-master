d154eb14441fc65a06d703abcc5c3461ff226440
Thalïa SAHED <thalia.sahed@saintmichelannecy.fr>
2020-11-20 11:58:43 +0100

    classe famillefromage

---
+++ ModelLayer/Business/FamilleFromage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer.Business
+{
+    class FamilleFromage
+    {
+        private int _id;
+        private string _name;
+        private List<Fromage> _LesFromages;
+
+        public FamilleFromage()
+        {
+            _id = 0;
+            _name = "";
+            _LesFromages = new List<Fromage>();
+        }
+
+    }
+}
