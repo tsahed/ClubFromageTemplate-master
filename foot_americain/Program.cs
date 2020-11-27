3a01e861b1c12bbacd7007af2c92880214f6a43a
thalia sahed <thalia.sahed@saintmichelannecy.fr>
2020-11-27 08:30:44 +0100

    arborescence

---
+++ foot_americain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace foot_americain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
